770e399a2add0fe1fd759389359ee8448b0c0c67
benita-laakso <benitalaaksofi@yahoo.com>
2019-08-22 09:50:58 +0300

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  document.getElementById("smallCircleContainer").style.visibility= "visible";
+
+}
+function myFunction2() {
+  document.getElementById("circleAdded").style.visibility= "visible";
+}
+
+
+function testFunction(){
+  document.getElementById("MyElement").className = "MyClass";
+}
